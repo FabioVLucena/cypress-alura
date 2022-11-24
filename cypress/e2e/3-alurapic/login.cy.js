4327780f14fa9bda35d05c6ec2f7b144d93ed6a7
aeon <yasunofabio@outlook.com.br>
2022-11-24 00:40:05 -0400

    separa testes em arquivo de login e cadastro

---
+++ cypress/e2e/3-alurapic/login.cy.js
@@ -0,0 +1,26 @@
+describe('Login de usuarios alura pic', () => {
+
+    beforeEach(() => {
+        cy.visit('https://alura-fotos.herokuapp.com')
+
+        cy.intercept('POST', 'https://apialurapic.herokuapp.com/user/login', {
+            statusCode: 400
+        }).as('stubPost')
+    })
+
+    it('fazer login de usuario valido', () => {
+        let username = Cypress.env("userName");
+        let password = Cypress.env("password");
+
+        cy.login(username, password);
+        cy.wait('@stubPost')
+        cy.contains('a', '(Logout)').should('be.visible');
+    })
+
+    it('fazer login de usuario invalido', () => {
+        cy.login('jacqueline', '1234');
+        cy.on('window:alert', (str) => {
+            expect(str).to.equal('Invalid user name or password')
+        })
+    })
+})
